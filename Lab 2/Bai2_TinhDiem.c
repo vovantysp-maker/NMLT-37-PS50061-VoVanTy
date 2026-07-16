d8361de775c0d5c823ef2e878d62a8fe9bcb2c2e
vovantysp-maker <vovantysp@gmail.com>
2026-07-16 10:21:35 +0700

    Hoan Thanh Bai2 Lab 2

---
+++ Lab 2/Bai2_TinhDiem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int toan, ly, hoa;
+    printf("Nhap diem mon toan: ");
+    scanf("%d", &toan);
+    printf("Nhap diem mon ly: ");
+    scanf("%d", &ly);
+    printf("Nhap diem mon hoa: ");
+    scanf("%d", &hoa);
+    float diemTB = (toan*3 + ly*2 + hoa) / 6.0;
+    printf("Diem trung binh la: %.2f\n", diemTB);
+    return 0;
+}
